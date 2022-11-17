47038296c1eb07301cc67bbdd6023d2ba47f8ad0
jirkaORIGINAL <jirik.ferus@gmail.com>
2022-11-17 15:23:48 +0100

    Update PlayerLook.cs
    jen jsem opravil PlayerLook bug

--- LowPoly Chujka/Assets/Scripts/PlayerLook.cs
+++ LowPoly Chujka/Assets/Scripts/PlayerLook.cs
@@ -14,7 +14,7 @@ public class PlayerLook : MonoBehaviour
     {
         float mouseX = input.x;
         float mouseY = input.y;
-        xRotation += (mouseY * Time.deltaTime) * ySensitivity;
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
